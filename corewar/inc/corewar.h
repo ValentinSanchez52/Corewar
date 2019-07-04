2000960a6dd633aed1fd4c2deebe81ab6f7e66bb
mbeilles <mbeilles@vente-privee.com>
2019-07-04 19:08:37 +0200

    New main

--- corewar/inc/corewar.h
+++ corewar/inc/corewar.h
@@ -6,7 +6,7 @@
 /*   By: mbeilles <mbeilles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/25 13:03:56 by mbeilles          #+#    #+#             */
-/*   Updated: 2019/06/25 13:05:06 by mbeilles         ###   ########.fr       */
+/*   Updated: 2019/07/04 17:32:59 by mbeilles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,18 @@
 
 # include "bboa.h"
 
+typedef enum		e_option_type
+{
+	OPT_NONE,
+	OPT_DOUBLE,
+	OPT_SINGLE,
+	OPT_MAX
+}					t_option_type;
+
 typedef struct		s_option
 {
 	char			*option[16];
+	t_option_type	type[16];
 	uint32_t		length;
 	t_opt_match		match;
 }					t_option;
--- corewar/src/main.c
+++ corewar/src/main.c
@@ -6,7 +6,7 @@
 /*   By: mbeilles <mbeilles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/25 14:39:16 by mbeilles          #+#    #+#             */
-/*   Updated: 2019/07/01 15:44:48 by njiall           ###   ########.fr       */
+/*   Updated: 2019/07/04 18:25:18 by mbeilles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,42 @@
 #include "bboa.h"
 #include "libft.h"
 
+// Generic help function
 static t_bboa_state			help(t_arg_array *args) {
 	return (BBOA_RS_DISPLAY_USAGE);
+}
+
+// Use this for debug purposes
+static t_bboa_state			print_args(t_arg_array *args) {
+	t_arg_token				token;
+	uint32_t				i;
+
+	i = -1;
+	while (++i < args->len)
+	{
+		token = args->array[i];
+		switch (token.type) {
+			default:
+				printf("Arg [NONE]: %p\n", token.none_token.data);
+				break ;
+			case BBOA_AT_STRING:
+				printf("Arg [STRG]: %s\n", token.string_token.string);
+				break ;
+			case BBOA_AT_NUMBER:
+				printf("Arg [NUMB]: %f\n", token.number_token.number);
+				break ;
+			case BBOA_AT_BOOLEAN:
+				printf("Arg [BOOL]: %s\n", (token.bool_token.boolean ? "true" : "false"));
+				break ;
+		}
+	}
+	return (BBOA_RS_OK);
 }
 
 static const t_option		g_options[] = {
 	(t_option){
 		.option = {"help", "h"},
+		.type = {OPT_DOUBLE, OPT_SINGLE},
 		.length = 2,
 		.match = (t_opt_match){
 			.func = &help,
@@ -33,19 +62,21 @@ static const t_option		g_options[] = {
 	},
 	(t_option){
 		.option = {"dump", "d"},
+		.type = {OPT_SINGLE, OPT_SINGLE},
 		.length = 2,
 		.match = (t_opt_match){
-			.func = &help,
+			.func = &print_args,
 			.types = {BBOA_AT_NUMBER},
 			.arg_count = 1,
 			.desc = "Dump the arena state after n cycles"
 		}
 	},
 	(t_option){
-		.option = {"number", "n"},
-		.length = 2,
+		.option = {"number", "num", "n"},
+		.type = {OPT_DOUBLE, OPT_DOUBLE, OPT_SINGLE},
+		.length = 3,
 		.match = (t_opt_match){
-			.func = &help,
+			.func = &print_args,
 			.types = {BBOA_AT_NUMBER, BBOA_AT_STRING},
 			.arg_count = 2,
 			.desc = "Sets next champion's number in args"
@@ -56,17 +87,21 @@ static const t_option		g_options[] = {
 static inline char			**parse_options(int c, char **v, void *data)
 {
 	static t_opt_patterns	patterns;
-	t_option				*options;
 	uint32_t				i;
 	uint32_t				j;
 
-	patterns = (t_opt_patterns){.map = ft_hashmap_create(), .data = data};
+	patterns = (t_opt_patterns){
+			.mapd = ft_hashmap_create(),
+			.maps = ft_hashmap_create(),
+			.data = data
+	};
 	i = ~0u;
 	while (++i < sizeof(g_options) / sizeof(t_option))
 	{
 		j = ~0u;
 		while (++j < g_options[i].length)
-			ft_hashmap_set(patterns.map,
+			ft_hashmap_set((g_options[i].type[j] == OPT_SINGLE) ?
+					patterns.maps : patterns.mapd,
 					(uint8_t*)g_options[i].option[j],
 					(t_opt_match*)&g_options[i].match);
 	}
@@ -79,15 +114,8 @@ int					main(int c, char **v)
 	int				i;
 	int				data;
 
-	i = 0;
-	if (v++ && (--c || 1) && (i = parse_options(c - i, v + i, &data) - v) >= c)
-		if (!(file = ft_get_fd_content(0)))
-			return (1);
-		else
-			printf("wow");
-	else if (!(file = ft_get_file_content(v[i])))
-		return (1);
-	else
-		printf("wow");
+	v++;
+	c--;
+	i = parse_options(c, v, &data) - v;
 	return (0);
 }
