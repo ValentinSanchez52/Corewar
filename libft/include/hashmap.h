7fd962feef373a6dfcec1b4e71f36c044a46a7ba
mbeilles <mbeilles@vente-privee.com>
2019-07-16 11:28:05 +0200

    Fixed header

--- libft/include/hashmap.h
+++ libft/include/hashmap.h
@@ -6,7 +6,7 @@
 /*   By: mbeilles <mbeilles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/15 20:09:57 by mbeilles          #+#    #+#             */
-/*   Updated: 2019/06/14 14:57:44 by mbeilles         ###   ########.fr       */
+/*   Updated: 2019/07/16 11:27:51 by mbeilles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,8 @@ typedef struct		s_hashmap
 	uint64_t		length;
 }					t_hashmap;
 
-t_hashmap			*ft_hashmap_create();
+t_hashmap			*ft_hashmap_create(void);
+void				ft_hashmap_destroy(t_hashmap *map);
 t_hash_entry		*ft_hash_entry_create(const uint8_t *key, void *value);
 uint16_t			ft_hash_djb2(const uint8_t *str);
 
@@ -48,6 +49,6 @@ void				*ft_hashmap_set(
 void				*ft_hashmap_remove(t_hashmap *map, const uint8_t *key);
 t_hash_entry		*ft_hashmap_iterate(t_hashmap *map, uint16_t *interator,
 										uint32_t *bucket_iterator);
-void				ft_hash_print(t_hashmap *map);
+void				ft_hashmap_print(t_hashmap *map);
 
 #endif
