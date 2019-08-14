7969b866eae3b21bc516fdcc154d99d8325ae471
Valentin Sanchez <valentinsanchez52@gmail.com>
2019-08-14 19:42:48 +0200

    fixed bugs on basic operations

--- corewar/src/instructions/zjmp.c
+++ corewar/src/instructions/zjmp.c
@@ -6,7 +6,7 @@
 /*   By: vsanchez <vsanchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/26 04:45:45 by vsanchez          #+#    #+#             */
-/*   Updated: 2019/08/10 12:04:02 by vsanchez         ###   ########.fr       */
+/*   Updated: 2019/08/14 16:27:12 by vsanchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void				op_zjmp(t_process *proc)
 {
 	/*printf("Process: global_offset %d, pc %d", proc->global_offset, proc->pc);*/
 	if (proc->carry && proc->op.args[0])
-		proc->pc += ((int16_t)proc->op.args[0] % COR_IDX_MOD);
+		proc->pc += (int16_t)proc->op.args[0] % COR_IDX_MOD;
 	else
 		proc->pc += proc->op.physical_size;
 	/*printf(" --------> proc_pc %d\n", proc->pc);*/
