a5069cbbfba3250e30825a9bbc8aaf130ab7f185
Valentin Sanchez <valentinsanchez52@gmail.com>
2019-08-20 11:11:27 +0200

    lldi works now

--- corewar/src/instructions/lldi.c
+++ corewar/src/instructions/lldi.c
@@ -6,7 +6,7 @@
 /*   By: vsanchez <vsanchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/26 07:17:31 by vsanchez          #+#    #+#             */
-/*   Updated: 2019/08/19 19:48:37 by vsanchez         ###   ########.fr       */
+/*   Updated: 2019/08/20 11:06:41 by vsanchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,26 +25,30 @@ static uint32_t		get_value(t_process *proc, uint8_t arg)
 	return (value);
 }
 
+/*
+**	Loads to the register given (third arg) the 4 bytes memory cell of
+**	PC + value_1 + value_2 given by a direct number or an adress
+*/
+
 void				op_lldi(t_process *proc)
 {
-	uint32_t		value_1;
-	uint32_t		value_2;
-	uint8_t			reg_id;
+	uint32_t				value_1;
+	uint32_t				value_2;
 
-	if (!((reg_id = proc->op.args[2]) && reg_id <= 16))
-		return ;
 	if (proc->op.types[0] == COR_ARG_REG && proc->op.args[0]
 			&& proc->op.args[0] <= 16)
 		value_1 = get_reg_value(proc, proc->op.args[0]);
-	else if (!(value_1 = get_value(proc, 0)))
-		return ;
+	else if (proc->op.types[0] == COR_ARG_DIR)
+		value_1 = (uint16_t)proc->op.args[0];
+	else if (proc->op.types[0] == COR_ARG_IND)
+		value_1 = get_mem_value(proc->global_offset + proc->pc
+				+ ((uint16_t)proc->op.args[0] % COR_IDX_MOD), COR_REG_SIZE);
 	if (proc->op.types[1] == COR_ARG_REG && proc->op.args[1]
 			&& proc->op.args[1] <= 16)
 		value_2 = get_reg_value(proc, proc->op.args[1]);
 	else if (proc->op.types[1] == COR_ARG_DIR)
 		value_2 = (uint16_t)proc->op.args[1];
-	else
-		return ;
-	reg_set_value(proc, reg_id, get_mem_value(proc->global_offset + proc->pc
+	reg_set_value(proc, proc->op.args[2],
+			get_mem_value(proc->global_offset + proc->pc
 				+ (int16_t)(value_1 + value_2), COR_REG_SIZE));
 }
